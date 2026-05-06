6bf30478fd4808534aa8ff92ee76e5b020ac53dd
Joaquin Bejar <jb@taunais.com>
2026-05-06 20:18:14 +0200

    Ignore target dir

--- target/debug/deps/deribit_mcp-1ea3cb0c8e6a579c.d
+++
@@ -1,7 +0,0 @@
-/Users/joaquin/Repos/Local/Deribit/deribit-mcp/target/debug/deps/deribit_mcp-1ea3cb0c8e6a579c.d: src/lib.rs
-
-/Users/joaquin/Repos/Local/Deribit/deribit-mcp/target/debug/deps/libderibit_mcp-1ea3cb0c8e6a579c.rlib: src/lib.rs
-
-/Users/joaquin/Repos/Local/Deribit/deribit-mcp/target/debug/deps/libderibit_mcp-1ea3cb0c8e6a579c.rmeta: src/lib.rs
-
-src/lib.rs:
--- target/debug/libderibit_mcp.d
+++
@@ -1,1 +0,0 @@
-/Users/joaquin/Repos/Local/Deribit/deribit-mcp/target/debug/libderibit_mcp.rlib: /Users/joaquin/Repos/Local/Deribit/deribit-mcp/target/package/deribit-mcp-0.1.0/src/lib.rs
